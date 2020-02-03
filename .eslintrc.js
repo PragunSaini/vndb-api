81ef8ef2715d4c21af51162947634e7e8d7cecb2
Pragun Saini <pragunsaini117@gmail.com>
2020-02-03 21:49:32 +0530

    Project Setup

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  rules: {},
+}
---
+++ __tests__/index.ts
@@ -0,0 +1,5 @@
+import { Greeter } from '../src/index'
+
+test('My Greeter', () => {
+  expect(Greeter('Pragun')).toBe('Hello, Pragun')
+})
