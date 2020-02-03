2963382edcb8bb139deb4c27835ac7be8bac86aa
Pragun Saini <pragunsaini117@gmail.com>
2020-02-03 22:01:16 +0530

    Setup test

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,5 +1,9 @@
-import { Greeter } from '../src/index'
+import Hello, { Greeter } from '../src/index'
 
 test('My Greeter', () => {
   expect(Greeter('Pragun')).toBe('Hello, Pragun')
+})
+
+test('My Hello', () => {
+  expect(Hello('Pragun')).toBe('Hullo, Pragun')
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export const Greeter = (name: string): string => `Hello, ${name}`
+
+const Hello = (name: string): string => `Hullo, ${name}`
+
+export default Hello
